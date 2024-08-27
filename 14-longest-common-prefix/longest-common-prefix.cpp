42f7001e9efb94ea9b023b47220433c2834e784a
Gowri N. <69057236+GowNanSA@users.noreply.github.com>
2024-08-26 21:01:41 -0500

    Time: 7 ms (27.33%) | Memory: 11.4 MB (27.76%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,42 @@
+#include <cstring>
+#include <string>
+class Solution {
+public:
+
+    // function to split the  word into an array 
+
+    char* split(string& str){
+        char* arr = new char[str.size()+1];
+        strcpy(arr, str.c_str()); 
+        return arr;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = "";
+
+        // return "" if nothing
+        if(strs.empty()){
+            return prefix; 
+        }
+
+        // sort based on alphabetical order
+        sort(strs.begin(), strs.end());
+
+        // store first and last values 
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
+
+        // take the one that is minimum size
+        int minWord = min(first.size(), last.size());
+
+        for(int i = 0; i<minWord;i++){
+            if(first[i] == last[i]){ // compare each character
+                prefix += first[i]; // append if it the characters are the same 
+            }
+            else{
+                break;
+            }
+        }
+
+        return prefix;
+    }
+};
